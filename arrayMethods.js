5a6bc21d077df70b6ad47d9c236ba1760e4c70b9
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-17 18:24:29 +0200

    Update arrayMethods.js
    entriesFn done but it needs some more refactoring, so that next function implementation wasn't repeated.

--- arrayMethods.js
+++ arrayMethods.js
@@ -12,7 +12,7 @@
 // Stwórz funkcje, które będą działać identycznie co metody wbudowane
 // ale będą działać przy pomocy pętli for lub while
 
-const sampleArray = [1,2,3,4,5,6,7,8,9,10];
+const sampleArray = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 
 function mapFn(array, callback) {
@@ -82,4 +82,37 @@ function someFn(array, callback) {
 const isAnyElementBiggerThan5 = someFn(sampleArray, element => element > 5);
 console.log(isAnyElementBiggerThan5);
 
-function entriesFn(array){}
+function entriesFn(array) {
+    const iterator = {
+        index: -1,
+        arr: array,
+        next() {
+            this.index++;
+            return this.index >= this.arr.length 
+            ? { value: undefined, done: true }
+            : { value: [this.index, this.arr[this.index]], done: false}
+        },
+    
+        [Symbol.iterator]() {
+            let index = -1;
+            const arr = this.arr;
+    
+            return {
+                next() {
+                    index++;
+                    return index >= arr.length 
+                    ? { value: undefined, done: true } 
+                    : {value: [index, arr[index]], done: false };
+                }
+            }
+        }
+    }
+    return iterator;
+};
+ 
+iterator = entriesFn(sampleArray);
+console.log(iterator.next())
+
+for (let e of iterator) {
+    console.log(e);
+  }
