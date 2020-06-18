4349d1edd9560db0a7e9388fe35439490d28cddc
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-18 15:59:32 +0200

    Changed validators.

--- commonFunctions.js
+++ commonFunctions.js
@@ -1,28 +1,11 @@
 export function validateArrayType(array) {
-    try {
-        if(!Array.isArray(array)) {
-            throw TypeError("Provided array variable isn't of type array");
-        }
-    }
-    catch(error) {
-        if(error instanceof TypeError) {
-            console.error(error.stack);
-        }
-    }
+    if(!Array.isArray(array)) {
+        throw TypeError("Provided array variable isn't of type array");
 }
 
 export function validateCallbackType(callback) {
-    try {
-        if(typeof callback !== "function"){
-            throw TypeError("Provided callback is not a function");
-        }
-    }
-    catch {
-        if(error instanceof TypeError) {
-            console.error(error.stack);
-        }
-    }
-    
+    if(typeof callback !== "function"){
+        throw TypeError("Provided callback is not a function");  
 }
 
 export function validate(array, callback){ 
