4553aae50f2cb446d078d987eb6b21b5e692b13b
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-20 16:06:43 +0200

    Another excercise done.

--- FunctionalProgrammingExcercises.js
+++ FunctionalProgrammingExcercises.js
@@ -123,6 +123,8 @@ console.log(arrayEvenNumbers);
 //Functional Programming excercises
 const polishAlphabetArray = Array.from('aąbcćdeęfghijklłmnńoóprsśtuwxyzźż');
 
+//Excercise: stwórz sobie array z literami polskiego alfabetu a potem stwórz kod na wszystkie metody for (...)
+
 
 
 // Excercise: korzystając z funkcji .reduce stwórz agregację liter alfabetu (...)
@@ -160,3 +162,43 @@ const createArrayOfArrays = function(array) {
 const polishAlphabetArrayOfArrays = createArrayOfArrays(polishAlphabetArray);
 console.log(polishAlphabetArrayOfArrays);
 
+//Excercise:korzystając z funkcji .filter stwórz funkcję filterWith(arr, filter) filtrowanie arraya z obiektami po stringu (...)
+function Person(name, age, animal) {
+    this.name = name;
+    this.age = age;
+    this.animal = animal;
+}
+
+const personsArray = [
+    new Person('Kait', 23, 'cat'), 
+    new Person('Marcus', 19, 'dog'), 
+    new Person('Merry', 10, 'hamster'), 
+    new Person('Gregory', 36, 'dolphin'), 
+    new Person('Savannah', 53, 'guinea pig'), 
+    new Person('Elise', 74, 'chinchilla'),
+    new Person('Mick', 29, 'cat')
+];
+
+const filterWith = function(arr, filter) {
+    const filterLength = filter.length;
+    let filteredArray = [];
+
+    if(filterLength === 0) {
+       return filteredArray = arr.filter(element => element.age > 30);
+    }
+
+    if(filterLength <= 3) {
+        return arr;
+    }
+
+    if(filterLength > 3) {
+        return filteredArray = arr.filter(element => element.name.startsWith('M') && element.age > 25 && element.animal === 'cat');
+    }
+}
+
+const filterWholeArray = filterWith(personsArray, '');
+const filterNothing = filterWith(personsArray, 'no');
+const filterByObjectsKeys = filterWith(personsArray, 'keys');
+console.log(filterNothing);
+console.log(filterWholeArray);
+console.log(filterByObjectsKeys);
