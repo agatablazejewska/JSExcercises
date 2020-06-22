e58f8a064f51425dc07650e6dcaaab458ef5954d
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-22 16:24:51 +0200

    Changed name of file cause it contains only one excercise now

--- FPExcercise1.js
+++ FPExcercise1.js
@@ -1,6 +1,5 @@
 import * as commonFunctions from './commonFunctions.js';
 
-// Excercise 1
 const calculateAge = function(birthYear) {
     birthYear = parseInt(birthYear);
     commonFunctions.validateNumber(birthYear);
@@ -11,8 +10,6 @@ const calculateAge = function(birthYear) {
 
 console.log(calculateAge(1996));
 
-//Functional Programming excercises
-const polishAlphabetArray = Array.from('aąbcćdeęfghijklłmnńoóprsśtuwxyzźż');
 
 
 
