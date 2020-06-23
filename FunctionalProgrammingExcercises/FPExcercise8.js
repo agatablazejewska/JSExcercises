e1af9d2e419c9e5c60c31fc2101b352d2315d022
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-23 18:37:13 +0200

    Refactoring. Taking random value from array made more generic.

--- FunctionalProgrammingExcercises/FPExcercise8.js
+++ FunctionalProgrammingExcercises/FPExcercise8.js
@@ -2,16 +2,11 @@ import * as commonFunctions from '../commonFunctions.js';
 import * as dataArrays from './FPExcercise8Data.js';
 import { randomIntFromInterval } from './AggregateAlphabetLettersExcercise.js';
 
-const takeRandomNameOrSurname = function(array) {
+const takeRandomValue = function(array) {
     commonFunctions.validateArrayType(array);
 
-    const randomIndex = randomIntFromInterval(0, 39);
+    const randomIndex = randomIntFromInterval(0, array.length);
     return array[randomIndex];
-}
-
-const takeRandomCountry = function() {
-    const randomIndex = randomIntFromInterval(0, 2);
-    return dataArrays.countries[randomIndex];
 }
 
 const generateRandomAge = function() {
@@ -23,16 +18,16 @@ const generatePhoneNumber = function() {
 }
 
 const generateHuman = function() {
-    return {
-        name: takeRandomNameOrSurname(dataArrays.firstNames),
-        surname: takeRandomNameOrSurname(dataArrays.surnames),
+    const human ={
+        name: takeRandomValue(dataArrays.firstNames),
+        surname: takeRandomValue(dataArrays.surnames),
         age: generateRandomAge(),
-        get email() {
-            `${this.name}${this.surname}@gmail.com`
-        },
-        country: takeRandomCountry(),
+        country: takeRandomValue(dataArrays.countries),
         phoneNr: generatePhoneNumber(),
     };
+    human.email = `${human.name.toLowerCase()}${human.surname.toLowerCase()}@gmail.com`;
+
+    return human;
 }
 
 
