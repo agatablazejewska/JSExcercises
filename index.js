928c59ed04a1a9570e30b9409d61ddac8628399d
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-27 00:12:45 +0200

    Added webpack

---
+++ index.js
@@ -0,0 +1,6 @@
+import { Contact } from "./OOPExcercises/Excercise1/Contact";
+import { generateHuman } from "./FunctionalProgrammingExcercises/FPExcercise8";
+
+console.log(new Contact("Aga", "Sokolowska", "agaS@gmail.com"));
+
+console.log(generateHuman());
