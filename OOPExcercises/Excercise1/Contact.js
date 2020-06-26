cbfa52067c26e1168a432aeabc238bc3e7086ae6
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-26 17:08:00 +0200

    Formatted using prettier

--- OOPExcercises/Excercise1/Contact.js
+++ OOPExcercises/Excercise1/Contact.js
@@ -1,67 +1,69 @@
-import * as commonFunctions from '../../commonFunctions.js';
+import * as commonFunctions from "../../commonFunctions.js";
 
 class Contact {
-    constructor(firstName, surname, email) {
-        this.setFirstName(firstName);
-        this.setSurname(surname);
-        this.setEmail(email);
-    }
+  constructor(firstName, surname, email) {
+    this.setFirstName(firstName);
+    this.setSurname(surname);
+    this.setEmail(email);
+  }
 
-    setEmail(value) {
-      commonFunctions.validateEmail(value);
-    
-      this._email = value;
-      this._modifyDate = this._todaysDate();
-    }
+  setEmail(value) {
+    commonFunctions.validateEmail(value);
 
-    getEmail(){
-        return this._email;
-    }
+    this._email = value;
+    this._modifyDate = this._todaysDate();
+  }
 
-    setFirstName(value) {
-        commonFunctions.validateString(value);
-       
-        this._firstName = value;
-        this._modifyDate = this._todaysDate();
-    } 
+  getEmail() {
+    return this._email;
+  }
 
-    getFirstName() {
-        return this._firstName;
-    }
+  setFirstName(value) {
+    commonFunctions.validateString(value);
 
-    setSurname(value) {
-        commonFunctions.validateString(value);
-       
-        this._surname = value;
-        this._modifyDate = this._todaysDate();
-    }
+    this._firstName = value;
+    this._modifyDate = this._todaysDate();
+  }
 
-    getSurname() {
-        return this._surname;
-    }
+  getFirstName() {
+    return this._firstName;
+  }
 
-    getModifyDate() {
-        return this._modifyDate;
-    }
+  setSurname(value) {
+    commonFunctions.validateString(value);
 
-    showContact() {
-        console.log(`Name: ${this.getFirstName()}
+    this._surname = value;
+    this._modifyDate = this._todaysDate();
+  }
+
+  getSurname() {
+    return this._surname;
+  }
+
+  getModifyDate() {
+    return this._modifyDate;
+  }
+
+  showContact() {
+    console.log(`Name: ${this.getFirstName()}
         Surname: ${this.getSurname()}
         E-mail: ${this.getEmail()}
         Last modified: ${this._formatDate(this.getModifyDate())}`);
+  }
+
+  _todaysDate() {
+    return new Date();
+  }
+
+  _formatDate(date) {
+    if (!date instanceof Date) {
+      throw Error("Provided value is not instance of Date");
     }
 
-    _todaysDate() {
-        return new Date();
-    }
-
-    _formatDate(date) {
-        if(!date instanceof Date) {
-            throw Error("Provided value is not instance of Date");
-        }
-
-        const fullDate = `${date.getDate()}-${(date.getMonth()+1)}-${date.getFullYear()}`;
-        const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-        return `${fullDate} ${time}`;
-    }
+    const fullDate = `${date.getDate()}-${
+      date.getMonth() + 1
+    }-${date.getFullYear()}`;
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    return `${fullDate} ${time}`;
+  }
 }
