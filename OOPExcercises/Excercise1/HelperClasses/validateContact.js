4dff0bc7a8438a1f82740ffd62dd749acac048d3
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-06-29 00:32:25 +0200

    Minor bugs with reference fixed.

--- OOPExcercises/Excercise1/HelperClasses/validateContact.js
+++ OOPExcercises/Excercise1/HelperClasses/validateContact.js
@@ -1,3 +1,5 @@
+import { Contact } from '../Contact';
+
 export function validateContact(contact) {
   if (!contact instanceof Contact) {
     throw new Error("Provided object is not a contact");
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { PhoneBook } from "./OOPExcercises/Excercise1/PhoneBook";
+import { Contact } from "./OOPExcercises/Excercise1/Contact";
 
 const phoneBook = new PhoneBook();
 
-phoneBook.createContact("Aga", "Soesk", "aserha@gmail.com");
-phoneBook.createContact("Marc", "Lokes", "marclos@gmail.com");
-phoneBook.createContact("Sid", "Mejer", "sidmej@gmail.com");
-phoneBook.createContact("Kasler", "Powkes", "kaslerpowkes@gmail.com");
+phoneBook.addContact(new Contact("Aga", "Soesk", "aserha@gmail.com"));
+phoneBook.addContact(new Contact("Marc", "Lokes", "marclos@gmail.com"));
+phoneBook.addContact(new Contact("Sid", "Mejer", "sidmej@gmail.com"));
+phoneBook.addContact(new Contact("Kasler", "Powkes", "kaslerpowkes@gmail.com"));
 
 phoneBook.showContacts();
