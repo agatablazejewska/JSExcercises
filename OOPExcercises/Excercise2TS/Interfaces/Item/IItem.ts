212d6ca0d2af89664a5854e8b22d3358bf060a1c
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-07-23 13:24:21 +0200

    Prepared Item class and all structures it depends on

--- OOPExcercises/Excercise2TS/Interfaces/Item/IItem.ts
+++ OOPExcercises/Excercise2TS/Interfaces/Item/IItem.ts
@@ -1,8 +1,11 @@
-import { IHasPrice } from "../Prices/IHasPrice";
 import { Categories } from "../../Utilities/Categories";
-import { IHasDiscount } from "../Prices/IHasDiscount";
+import { IUpdatableAndReadable } from "../../../Common/IUpdatableAndReadable";
+import { IHasID } from "../../../Common/IHasID";
 
-export interface IItem extends IHasPrice, IHasDiscount {
-    name : string;
-    category: Categories;  
+export interface IItem extends IUpdatableAndReadable, IHasID {
+    readonly name : string;
+    readonly price : number;
+    readonly category : Categories;  
+    readonly discount : number;
+    getPriceAfterDiscount() : number;
 }
--- OOPExcercises/Excercise2TS/Item/Item.ts
+++ OOPExcercises/Excercise2TS/Item/Item.ts
@@ -1,11 +1,9 @@
 import uuid4 from "uuid4";
-import { IHasPrice } from "../Interfaces/Prices/IHasPrice";
 import { IItem } from "../Interfaces/Item/IItem";
 import { Categories } from "../Utilities/Categories";
-import { IHasDiscount } from "../Interfaces/Prices/IHasDiscount";
+
 
 export class Item implements IItem {
-    private readonly _defaultDiscountIfWrongProvided = 20;
     private readonly _id: string;
     name: string;
     category: Categories;
@@ -20,29 +18,32 @@ export class Item implements IItem {
         this.discount = this._validateOrChangeToDefault(discount);
     }
 
-    update<IHasDiscount>(source: IHasDiscount) : void
-    update<IHasPrice>(source: IHasPrice): void {
+    get id() {
+        return this._id;
+    }
+
+    getPriceAfterDiscount() : number {
+        return (100 - this.discount) / 100 * this.price;
+    }
+
+    update<IItemDataOptional>(source: IItemDataOptional) : void {
         Object.assign(this, source);
     }
 
-    show(): void {
+    show() : void {
         console.log(`${this.name}
          ${this.price} $`);
     }
 
-    showAllInfo(): void {
+    showAllInfo() : void {
         console.log(`${this.name}
          Price: ${this.price} $
          Category: ${this.category}
          Discount: ${this.discount}%`);
     }
 
-    get ID() {
-        return this._id;
-    }
-
     _validateOrChangeToDefault(discount : number) : number {
-        return (discount >= 0 && discount <= 100) ? discount : this._defaultDiscountIfWrongProvided;
+        return (discount >= 0 && discount <= 100) ? discount : 0;
     }
     
 }
