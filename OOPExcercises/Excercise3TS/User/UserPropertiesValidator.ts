728bdaaa621698a356f7252383b33067c0d20298
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-07-27 21:05:28 +0200

    Prepared User data validator

--- OOPExcercises/Excercise3TS/User/UserPropertiesValidator.ts
+++ OOPExcercises/Excercise3TS/User/UserPropertiesValidator.ts
@@ -1,7 +1,8 @@
 import { CommonValidator } from "../../Common/CommonValidator";
+import moment from 'moment';
 
 export abstract class UserPropertiesValidator {
-    static validateNameSurname(name: string, surname: string) {
+    static validateNameSurname(name: string, surname: string): void {
         try {
             CommonValidator.validateEmptyString(name); 
             CommonValidator.validateEmptyString(surname);
@@ -11,7 +12,30 @@ export abstract class UserPropertiesValidator {
         }
     }
 
-    static validateEmail(email: string) {
+    static validateEmail(email: string): void {
+        CommonValidator.validateEmptyString(email);
         CommonValidator.validateEmail(email);
+    }
+
+    static validatePassword(password: string): void {
+        CommonValidator.validatePassword(password);
+    }
+
+    static validateAndFormatDateOfBirth(date: string, dateCurrentFormat: string): string {
+        CommonValidator.validateEmptyString(date);
+        CommonValidator.validateEmptyString(dateCurrentFormat);
+
+        const momentDate = moment(date, dateCurrentFormat, true);
+        if(UserPropertiesValidator._isDateInvalid(momentDate)) {
+            throw new Error("Provided date is not valid");
+        }
+
+        let formattedDate = momentDate.format('MM-DD-YYYY');
+
+        return formattedDate;
+    }
+
+    private static _isDateInvalid(momentDate: moment.Moment) {
+        return !momentDate.isValid() || momentDate.year() <= 1900 || momentDate.year() > moment().year();
     }
 }
