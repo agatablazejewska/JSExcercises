b546d6322690298e4732ae464c7ac210ffb486dd
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-07-29 17:33:48 +0200

    First draft of IUser interface

--- OOPExcercises/Excercise3TS/Utilities/Interfaces/User/IUser.ts
+++ OOPExcercises/Excercise3TS/Utilities/Interfaces/User/IUser.ts
@@ -1,8 +1,9 @@
 import { Gender } from "../../Enums/Gender";
 import { AccessLevels } from "../../Enums/AccessLevel";
 import { IUpdatableAndReadable } from "../../../../Common/IUpdatableAndReadable";
+import { IHasID } from "../../../../Common/IHasID";
 
-export interface IUser extends IUpdatableAndReadable {
+export interface IUser extends IUpdatableAndReadable, IHasID {
     readonly name: string;
     readonly surname: string;
     readonly email: string;
@@ -10,4 +11,5 @@ export interface IUser extends IUpdatableAndReadable {
     readonly gender: Gender; 
     accessLevel: AccessLevels;
     canLogin(email: string, password: string): boolean;
+    updatePassword(password: string): void;
 }
