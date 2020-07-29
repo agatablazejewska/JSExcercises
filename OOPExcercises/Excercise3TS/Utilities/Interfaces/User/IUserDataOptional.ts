12d43fe6eb00df0480e4be6e3fc53c06e1fdd050
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-07-29 17:33:18 +0200

    Removed password from optional data - private properties wouldn't get updated anyway

--- OOPExcercises/Excercise3TS/Utilities/Interfaces/User/IUserDataOptional.ts
+++ OOPExcercises/Excercise3TS/Utilities/Interfaces/User/IUserDataOptional.ts
@@ -3,6 +3,5 @@ import { AccessLevels } from "../../Enums/AccessLevel";
 export interface IUserDataOptional {
     surname?: string;
     email?: string;
-    _password?: string;
     accessLevel?: AccessLevels;
 }
