239030e808e44d49be1644d0ca231b38be6362a2
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-07-29 20:52:13 +0200

    Added mechanism for changing user access level
    Changing access level to admin requires new Admin object to be created.

--- OOPExcercises/Excercise3TS/Admin/Admin.ts
+++ OOPExcercises/Excercise3TS/Admin/Admin.ts
@@ -3,6 +3,8 @@ import { IAdmin } from "../Utilities/Interfaces/Admin/IAdmin";
 import { Gender } from "../Utilities/Enums/Gender";
 import { AccessLevels } from "../Utilities/Enums/AccessLevel";
 import { DateOfBirth } from "../Utilities/Types/DateOfBirth";
+import { UserAndPassword } from "../Utilities/Types/UserAndPassword";
+import { IUser } from "../Utilities/Interfaces/User/IUser";
 
 
 export class Admin extends User implements IAdmin {
@@ -11,23 +13,38 @@ export class Admin extends User implements IAdmin {
     constructor(name: string, surname: string, email: string, dateOfBirth: DateOfBirth, dateOfBirthCurrentFormat: string, 
     gender: Gender, password: string) {
         super(name, surname, email, dateOfBirth, dateOfBirthCurrentFormat, gender, password);
+
         this.accessLevel = AccessLevels.Admin;
     }
 
     modifyUserPassword(userId: string, newPassword: string): void {
-        const user = this._findUserById(userId);
-        user?.updatePassword(newPassword);
+        const userData = this._findUserDataById(userId);
+        userData?.user.updatePassword(newPassword);
     }
 
     modifyUserAccessLevel(userId: string): void {
-        //const user = this._findUserById(userId);
-
-        //should also think of creating new object with the same data but new class -> so that when user gets admin accessLevel,
-        //it should actually become Admin instance
+        const userData = this._findUserDataById(userId);
+        const user = userData?.user;
+        if (user) {
+            this._createNewAdminAndAddToUserStorage(user, userData!);
+            this._removeExistingUserFromUsersArray(userId);
+        }
     }
 
-    private _findUserById(userId: string) {
-        return this._usersStorage.users.find(u => u.user.id === userId)?.user;
+    private _removeExistingUserFromUsersArray(userId: string) {
+        const index = this._usersStorage.users.findIndex(element => element.user.id === userId);
+
+        if (index > -1) {
+            this._usersStorage.users.splice(index, 1);
+        }
     }
-    
+
+    private _createNewAdminAndAddToUserStorage(user: IUser, userData: UserAndPassword): void {
+        let admin = new Admin(user.name, user.surname, user.email, user.dateOfBirth, this._finalDateFormat, user.gender, userData.password);
+        this._usersStorage.users.push({user: admin, password: userData.password});
+    }
+
+    private _findUserDataById(userId: string) : UserAndPassword | undefined {
+        return this._usersStorage.users.find(u => u.user.id === userId);
+    }   
 }
