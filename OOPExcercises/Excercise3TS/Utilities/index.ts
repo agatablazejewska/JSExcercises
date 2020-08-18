d76404ca46c147a8e13d598e6d574cbbd416267c
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-08-18 19:41:29 +0200

    exported interfaces and refactoring done

--- OOPExcercises/Excercise3TS/Utilities/index.ts
+++ OOPExcercises/Excercise3TS/Utilities/index.ts
@@ -1,14 +1,18 @@
-export { default as IAdmin }  from "./Interfaces/Admin/IAdmin";
-export { default as AccessLevels }  from "./Enums/AccessLevel";
-export { default as IHandleUsersStorage }  from "./Interfaces/UsersStorage/IHandleUsersStorage";
-export { default as UserAndPassword } from "./Types/UserAndPassword";
-export { default as UserConstructionData }  from "./Types/UserConstructionData";
-export { default as IUser } from "./Interfaces/User/IUser";
-export { default as Gender } from "./Enums/Gender";
-export { default as IUserDataOptional } from "./Interfaces/User/IUserDataOptional";
-export { default as DateOfBirth }  from "./Types/DateOfBirth";
-export { default as IChatRoom } from "./Interfaces/ChatRoom/IChatRoom";
-export { default as IMessage } from "./Interfaces/Message/IMessage";
-export { default as IChatRoomsStorageHandler } from "./Interfaces/ChatRoomsStorage/IChatRoomsStorageHandler";
-export { default as IHandleMessages } from "./Interfaces/ChatRoom/IHandleMessages";
-export { default as IGetRoom } from "./Interfaces/ChatRoomsStorage/IGetRoom";
+export { default as IAdmin } from './Interfaces/Admin/IAdmin';
+export { default as AccessLevels } from './Enums/AccessLevel';
+export {
+    default as IHandleUsersStorage,
+} from './Interfaces/UsersStorage/IHandleUsersStorage';
+export { default as UserAndPassword } from './Types/UserAndPassword';
+export { default as UserConstructionData } from './Types/UserConstructionData';
+export { default as IUser } from './Interfaces/User/IUser';
+export { default as Gender } from './Enums/Gender';
+export { default as IUserDataOptional } from './Interfaces/User/IUserDataOptional';
+export { default as DateOfBirth } from './Types/DateOfBirth';
+export { default as IChatRoom } from './Interfaces/ChatRoom/IChatRoom';
+export { default as IMessage } from './Interfaces/Message/IMessage';
+export { default as IChatRoomsStorageHandler } from './Interfaces/ChatRoomsStorage/IChatRoomsStorageHandler';
+export { default as IGetRoom } from './Interfaces/ChatRoomsStorage/IGetRoom';
+export { default as IChatUser } from './Interfaces/Chat/IChatUser';
+export { default as IChatAdmin } from './Interfaces/Chat/IChatAdmin';
+
