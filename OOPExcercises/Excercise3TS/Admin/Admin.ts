9905425ebcd920f3435ce7e477876047a7a37c5b
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-08-19 18:21:08 +0200

    Removed chat instance from Admin.
    Admin no longer has direct access to some of chat methods.

--- OOPExcercises/Excercise3TS/Admin/Admin.ts
+++ OOPExcercises/Excercise3TS/Admin/Admin.ts
@@ -1,52 +1,13 @@
-import { Chat } from '../Chat/Chat';
-import { ChatRoom } from '../ChatRoom/ChatRoom';
 import { User } from "../User/User";
 import {
-    IAdmin,
     AccessLevels,
-    UserConstructionData, IChatAdmin,
+    UserConstructionData,
 } from '../Utilities';
 
-export class Admin extends User implements IAdmin {
-    private readonly _chatForAdmin: IChatAdmin;
-    accessLevel: AccessLevels;
-    
-    constructor(data: UserConstructionData, chat: IChatAdmin = new Chat()) {
+export class Admin extends User {
+    constructor(data: UserConstructionData) {
         super(data);
 
-        this._chatForAdmin = chat;
         this.accessLevel = AccessLevels.Admin;
-    }
-
-    modifyUserPassword(userId: string, newPassword: string): void {
-        this._chatForAdmin.updateUserPassword(userId, newPassword);
-    }
-
-    setUserAccessLevelToAdmin(userId: string): void {
-        this._chatForAdmin.userToAdmin(userId);
-    }    
-
-    setAdminAccessLevelToUser(adminId: string): void {
-        this._chatForAdmin.adminToUser(adminId);
-    }
-
-    banUserInRoom(roomId: string, userId: string): void {
-        this._chatForAdmin.banUserInRoom(roomId, userId);
-    }
-
-    createNewRoom(name: string, description: string): void {
-        this._chatForAdmin.addNewRoom(new ChatRoom(name, description));
-    }
-
-    deleteRoom(roomId: string): void {
-        this._chatForAdmin.deleteRoom(roomId);
-    }
-
-    deleteMessageInRoom(roomId: string, messageId: string): void {
-        this._chatForAdmin.removeMessageFromRoom(roomId, messageId);
-    }
-
-    removeUserFromRoom(roomId: string, userId: string): void {
-        this._chatForAdmin.removeUserFromRoom(roomId, userId);
     }
 }
--- OOPExcercises/Excercise3TS/Utilities/Interfaces/Admin/IAdmin.ts
+++ OOPExcercises/Excercise3TS/Utilities/Interfaces/Admin/IAdmin.ts
@@ -3,11 +3,4 @@ import  AccessLevels  from "../../Enums/AccessLevel";
 
 export default interface IAdmin extends IUser {
     accessLevel: AccessLevels;
-    modifyUserPassword(userId: string, newPassword: string): void;
-    setUserAccessLevelToAdmin(userId: string): void;
-    createNewRoom(name: string, description:string): void;
-    deleteRoom(roomId: string): void;
-    deleteMessageInRoom(roomId: string, messageId: string): void;
-    removeUserFromRoom(roomId: string, userId: string): void;
-    banUserInRoom(roomId: string, userId: string): void;
 }
