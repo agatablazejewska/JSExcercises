56e83c1b4234fd6241342ac843e5edc7d599cd73
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-08-20 19:27:19 +0200

    Changed name of class from Admin to Employee so it is more suitable for the current case

--- OOPExcercises/Excercise4TS/Employee.ts
+++ OOPExcercises/Excercise4TS/Employee.ts
@@ -1,7 +1,7 @@
 import { AccessLevels } from '../Excercise3TS/Utilities';
 import { User } from './User';
 
-export class Admin extends User {
+export class Employee extends User {
     constructor(name: string, surname: string) {
         super(name, surname);
 
