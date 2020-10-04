34b16be3640505ad4fe628d8bdb0171632bc3eb5
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-10-04 23:05:37 +0200

    Added configuration files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const config = {
+    entry: './index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.ts(x)?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.tsx',
+            '.ts',
+            '.js'
+        ]
+    }
+};
+
+module.exports = config;
