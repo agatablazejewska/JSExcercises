9ef8a8ec591f6bd972b2a81e76ab76065eda31ae
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-10-06 13:42:17 +0200

    Tests added
    Tests for exercise 3 and 4 from functional programming exercises added.

--- FunctionalProgrammingExcercises/FPExcercise3and4.js
+++ FunctionalProgrammingExcercises/FPExcercise3and4.js
@@ -1,7 +1,7 @@
 import * as commonFunctions from '../commonFunctions.js';
 
 // Excercise 3
-const arrayRandomValues = function(length) {
+export const arrayRandomValues = function(length) {
     commonFunctions.validateNumber(length);
     if(length < 0) {
         throw new Exception("Array length can't be less than 0.");
@@ -11,7 +11,7 @@ const arrayRandomValues = function(length) {
 }
 
 // Excercise 4
-const createArray10ArraysWithin = function() {
+export const createArray10ArraysWithin = function() {
     const basicArray = Array.from({length: 10});
     const arrayWithArraysInside = basicArray.map(() => arrayRandomValues(10));
 
