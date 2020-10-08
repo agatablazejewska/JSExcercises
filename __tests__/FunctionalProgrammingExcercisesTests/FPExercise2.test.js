68dcd411fd4a506528a2f8a6600452bc4e9d2ab6
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-10-08 18:24:45 +0200

    Tests refactoring
    Refactored tests for exercise2

--- __tests__/FunctionalProgrammingExcercisesTests/FPExercise2.test.js
+++ __tests__/FunctionalProgrammingExcercisesTests/FPExercise2.test.js
@@ -1,73 +1,72 @@
 import { canBuildRightAngledTriangle } from '../../FunctionalProgrammingExcercises/FPExcercise2';
 
 test(`Gets numbers that can be used to build right angled triangle. Should return true`, () => {
-    expect(canBuildRightAngledTriangle(3,4,5)).toBe(true);
-    expect(canBuildRightAngledTriangle(4,3,5)).toBe(true);
-    expect(canBuildRightAngledTriangle(5,3,4)).toBe(true);
-    expect(canBuildRightAngledTriangle(13,5,12)).toBe(true);
+    const sidesArray = [
+        { sideA: 3, sideB: 4, sideC: 5 },
+        { sideA: 3, sideB: 4, sideC: 5 },
+        { sideA: 3, sideB: 4, sideC: 5 },
+        { sideA: 3, sideB: 4, sideC: 5 }];
+
+    sidesArray.forEach(sides => expect(canBuildRightAngledTriangle(sides.sideA, sides.sideB, sides.sideC)).toBe(true));
 });
 
 test(`Gets numbers that can't be used to build right angled triangle. Should return false`, () => {
-    expect(canBuildRightAngledTriangle(2,9,5)).toBe(false);
-    expect(canBuildRightAngledTriangle(8,1,12)).toBe(false);
-    expect(canBuildRightAngledTriangle(3,3,3)).toBe(false);
+    const sidesArray = [
+        { sideA: 2, sideB: 9, sideC: 5 },
+        { sideA: 8, sideB: 1, sideC: 12 },
+        { sideA: 3, sideB: 3, sideC: 3 }];
+
+    sidesArray.forEach(sides => expect(canBuildRightAngledTriangle(sides.sideA, sides.sideB, sides.sideC)).toBe(false));
 });
 
 test(`One or more of provided numbers are equal to 0. Should throw an error.`,() => {
-    expect(() => { canBuildRightAngledTriangle(3,4,0) })
-        .toThrowError();
+    const sidesArray = [
+        { sideA: 3, sideB: 4, sideC: 0 },
+        { sideA: 0, sideB: 2, sideC: 5 },
+        { sideA: 3, sideB: 0, sideC: 5 },
+        { sideA: 0, sideB: 3, sideC: 0 },
+        { sideA: 0, sideB: 0, sideC: 0 }];
 
-    expect(() => { canBuildRightAngledTriangle(0,2,5) })
-        .toThrowError();
-
-    expect(() => { canBuildRightAngledTriangle(3,0,4) })
-        .toThrowError();
-
-    expect(() => { canBuildRightAngledTriangle(0,3,0) })
-        .toThrowError();
-
-    expect(() => { canBuildRightAngledTriangle(0,0,0) })
-        .toThrowError();
+    sidesArray.forEach(sides =>
+        expect( () => { canBuildRightAngledTriangle(sides.sideA, sides.sideB, sides.sideC) })
+            .toThrowError(`You can't build right angled triangle with any side equal to 0.`));
 });
 
 test(`Some of arguments provided are strings. Should throw a TypeError.`, () => {
-    expect(() => { canBuildRightAngledTriangle('3',4,5) })
-        .toThrow(TypeError);
+    const sidesArray = [
+        { sideA: '3', sideB: 4, sideC: 4 },
+        { sideA: 1, sideB: '2', sideC: 5 },
+        { sideA: 3, sideB: 2, sideC: '5' },
+        { sideA: 1, sideB: 3, sideC: 'six' }];
 
-    expect(() => { canBuildRightAngledTriangle(3,'4',5) })
-        .toThrow(TypeError);
-
-    expect(() => { canBuildRightAngledTriangle(3,4,'5') })
-        .toThrow(TypeError);
-
-    expect(() => { canBuildRightAngledTriangle(3,4,'six') })
-        .toThrow(TypeError);
+    sidesArray.forEach(sides =>
+        expect( () => { canBuildRightAngledTriangle(sides.sideA, sides.sideB, sides.sideC) })
+            .toThrow(TypeError));
 });
 
 test(`Some of arguments provided are negative values. Should throw an error.`, () => {
-    expect(() => { canBuildRightAngledTriangle(-3,4,5) })
-        .toThrowError();
+    const sidesArray = [
+        { sideA: -2, sideB: 9, sideC: 5 },
+        { sideA: 8, sideB: -1, sideC: 12 },
+        { sideA: 3, sideB: 3, sideC: -3 }];
 
-    expect(() => { canBuildRightAngledTriangle(3,-4,5) })
-        .toThrowError();
-
-    expect(() => { canBuildRightAngledTriangle(3,4,-5) })
-        .toThrowError();
+    sidesArray.forEach(sides =>
+        expect( () => { canBuildRightAngledTriangle(sides.sideA, sides.sideB, sides.sideC) })
+            .toThrowError(`Value should be a positive number.`));
 });
 
 test(`Some of arguments provided are not of type number. Should throw a TypeError.`, () => {
-    expect(() => { canBuildRightAngledTriangle(true,4,5) })
-        .toThrow(TypeError);
+    const typesArray = [true, null, undefined, NaN, [], { number: 4 }, '3'];
 
-    expect(() => { canBuildRightAngledTriangle(3,null,5) })
-        .toThrow(TypeError);
+    typesArray.forEach(t =>
+        expect( () => { canBuildRightAngledTriangle(t, 3, 5) })
+            .toThrow(TypeError));
 
-    expect(() => { canBuildRightAngledTriangle(3, undefined,-5) })
-        .toThrow(TypeError);
+    typesArray.forEach(t =>
+        expect( () => { canBuildRightAngledTriangle(2, t, 5) })
+            .toThrow(TypeError));
 
-    expect(() => { canBuildRightAngledTriangle(3, 4, NaN) })
-        .toThrow(TypeError);
-
-    expect(() => { canBuildRightAngledTriangle(3, { number: 4 },-5) })
-        .toThrow(TypeError);
+    typesArray.forEach(t =>
+        expect( () => { canBuildRightAngledTriangle(2, 3, t) })
+            .toThrow(TypeError));
 });
