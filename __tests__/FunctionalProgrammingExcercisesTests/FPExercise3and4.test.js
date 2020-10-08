115f98fa6912d46b6966711ea4a35482e02d59fa
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-10-08 18:37:52 +0200

    Tests improvement and refactoring
    Improved and refactored tests for exercise 3 and 4 from functional programming exercises

--- __tests__/FunctionalProgrammingExcercisesTests/FPExercise3and4.test.js
+++ __tests__/FunctionalProgrammingExcercisesTests/FPExercise3and4.test.js
@@ -4,25 +4,19 @@ test('Should create few arrays with random values inside and with provided lengt
     const array1 = arrayRandomValues(3);
     const array2 = arrayRandomValues(3);
 
-    const areSame = array1[0] === array2[0] || array1[1] === array2[1] || array1[2] === array2[2];
-
-    expect(areSame).toBe(false);
     expect(array1.length).toBe(3);
     expect(array2.length).toBe(3);
+    expect(array1).not.toEqual(array2);
 });
 
 test(`Provided length is not of type number. Should throw type error.`, () => {
-   expect(() => { arrayRandomValues('5') }).toThrow(TypeError);
-   expect(() => { arrayRandomValues('') }).toThrow(TypeError);
-   expect(() => { arrayRandomValues(null) }).toThrow(TypeError);
-   expect(() => { arrayRandomValues(undefined) }).toThrow(TypeError);
-   expect(() => { arrayRandomValues(NaN) }).toThrow(TypeError);
-   expect(() => { arrayRandomValues({ length: 5 }) }).toThrow(TypeError);
-   expect(() => { arrayRandomValues(true) }).toThrow(TypeError);
+    const typesArray = ['5', null, undefined, NaN, { length: 5 }, true, []];
+
+    typesArray.forEach(t => expect(() => { arrayRandomValues(t) }).toThrow(TypeError));
 });
 
 test(`Provided length is less than 0. Should throw an error.`, () => {
-    expect(() => { arrayRandomValues(-3) }).toThrowError();
+    expect(() => { arrayRandomValues(-3) }).toThrowError(`Array length can't be less than 0.`);
 });
 
 test(`Provided length is equal to 0. Should create empty array.`, () => {
@@ -36,14 +30,11 @@ test(`Create array with nested arrays inside. Should be of length 10 and nested 
     const array = createArray10ArraysWithin();
 
     expect(array.length).toBe(10);
-    expect(array[0].length).toBe(10);
-    expect(array[1].length).toBe(10);
-    expect(array[2].length).toBe(10);
-    expect(array[3].length).toBe(10);
-    expect(array[4].length).toBe(10);
-    expect(array[5].length).toBe(10);
-    expect(array[6].length).toBe(10);
-    expect(array[7].length).toBe(10);
-    expect(array[8].length).toBe(10);
-    expect(array[9].length).toBe(10);
+    
+    array.forEach((elem, index) => {
+        expect(elem.length).toBe(10);
+        if(index > 0) {
+            expect(array[0]).not.toEqual(elem);
+        }
+    });
 });
