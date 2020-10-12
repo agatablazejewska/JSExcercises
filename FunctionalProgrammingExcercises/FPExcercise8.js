8daba46eb10fb9cf0372018961cff5423b4ba7c3
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-10-12 15:39:41 +0200

    Changed one test and corresponding file to use mocking

--- FunctionalProgrammingExcercises/FPExcercise8.js
+++ FunctionalProgrammingExcercises/FPExcercise8.js
@@ -1,6 +1,7 @@
 import uuid4 from "uuid4";
 import * as commonFunctions from "../commonFunctions";
 import * as dataArrays from "./FPExcercise8Data.js";
+import * as thisModule from './FPExcercise8';
 
 export const takeRandomValue = function (array) {
   commonFunctions.validateArrayType(array);
@@ -23,11 +24,11 @@ export const generatePhoneNumber = function () {
 export const generateHuman = function () {
   const human = {
     _id: uuid4(),
-    name: takeRandomValue(dataArrays.firstNames),
-    surname: takeRandomValue(dataArrays.surnames),
-    age: generateRandomAge(),
-    country: takeRandomValue(dataArrays.countries),
-    phoneNr: generatePhoneNumber(),
+    name: thisModule.takeRandomValue(dataArrays.firstNames),
+    surname: thisModule.takeRandomValue(dataArrays.surnames),
+    age: thisModule.generateRandomAge(),
+    country: thisModule.takeRandomValue(dataArrays.countries),
+    phoneNr: thisModule.generatePhoneNumber(),
   };
   human.email = `${human.name.toLowerCase()}${human.surname.toLowerCase()}@gmail.com`;
 
--- __tests__/FunctionalProgrammingExcercisesTests/FPExercise8.test.js
+++ __tests__/FunctionalProgrammingExcercisesTests/FPExercise8.test.js
@@ -1,15 +1,13 @@
-import { when } from 'jest-when';
 import * as data from '../../FunctionalProgrammingExcercises/FPExcercise8Data';
-import { takeRandomValue, generateRandomAge, generatePhoneNumber, generateHuman } from '../../FunctionalProgrammingExcercises/FPExcercise8';
+import * as generateFunctions from '../../FunctionalProgrammingExcercises/FPExcercise8';
 
 describe(`Tests for function that takes random values from an array.`, () => {
-
     describe('Check if function returns correct results', () => {
 
         test(`Takes arrays with first names and surnames as argument. 
           Should return data that is present in the array.`, () => {
-           const name = takeRandomValue(data.firstNames);
-           const surname = takeRandomValue(data.surnames);
+           const name = generateFunctions.takeRandomValue(data.firstNames);
+           const surname = generateFunctions.takeRandomValue(data.surnames);
 
            expect(data.firstNames.includes(name)).toBe(true);
            expect(data.surnames.includes(surname)).toBe(true);
@@ -18,17 +16,16 @@ describe(`Tests for function that takes random values from an array.`, () => {
 
 
     describe(`Check if function responds properly to encountered errors`, () => {
-
         test(`Provided array is empty. Should throw an error.`, () => {
             const arr = [];
 
-            expect(() => { takeRandomValue(arr) }).toThrowError(`Provided array is empty.`);
+            expect(() => { generateFunctions.takeRandomValue(arr) }).toThrowError(`Provided array is empty.`);
         });
 
         test(`Provided argument is not an array. Should throw a TypeError.`, () => {
             const types = [null, undefined, 1234, 'arr', true, { array: [1,2,3] }, NaN];
 
-            types.forEach(t => expect(() => { takeRandomValue(t) })
+            types.forEach(t => expect(() => { generateFunctions.takeRandomValue(t) })
                 .toThrow(new TypeError(`Provided array variable isn't of type array`)));
         });
     });
@@ -36,18 +33,16 @@ describe(`Tests for function that takes random values from an array.`, () => {
 
 
 describe(`Tests for function that generates random age. Check if function returns correct results. `, () => {
-
     test(`Should generate random numbers in the range 18 to 85.`, () => {
-       const resultsArray = Array.from({ length: 100 }, () => generateRandomAge());
+       const resultsArray = Array.from({ length: 100 }, () => generateFunctions.generateRandomAge());
 
        resultsArray.forEach(element => expect(element >= 18 && element <= 85).toBe(true));
     });
 });
 
 describe(`Tests for function that generates random phone number.Check if function returns correct results.`, () => {
-
     test(`Should generate random phone number as string.`, () => {
-       const result = generatePhoneNumber();
+       const result = generateFunctions.generatePhoneNumber();
 
        expect(result.length).toBe(9);
        expect(typeof result === 'string').toBe(true);
@@ -56,11 +51,20 @@ describe(`Tests for function that generates random phone number.Check if functio
 });
 
 describe(`Tests for function that generates human object. Check if function returns correct results.`, () => {
+    let takeRandomValueSpy = null;
+    beforeAll(() => {
+        takeRandomValueSpy = jest.spyOn(generateFunctions, 'takeRandomValue');
+        takeRandomValueSpy.mockImplementation(() => 'mock');
+    });
+
+    afterAll(() => {
+        takeRandomValueSpy.mockRestore();
+    });
 
     test(`Should generate a human object with all necessary properties that are not empty/undefined.
      Email should consist of name and surname combined.`, () => {
         const propertiesArray = ['_id', 'name', 'surname', 'age', 'country', 'phoneNr', 'email'];
-        const result = generateHuman();
+        const result = generateFunctions.generateHuman();
 
         propertiesArray.forEach(p => {
             expect(result[p]).not.toBe(undefined);
@@ -68,7 +72,7 @@ describe(`Tests for function that generates human object. Check if function retu
             expect(result[p]).not.toBe('');
         });
 
-        expect(result.email).toBe(`${result.name.toLowerCase()}${result.surname.toLowerCase()}@gmail.com`);
+        expect(result.email).toBe(`mockmock@gmail.com`);
     });
 });
 
