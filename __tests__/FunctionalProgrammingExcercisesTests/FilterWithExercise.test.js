f32c335fcffc545ad5633e7f4f82dcd47c9ed4d0
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-10-30 20:11:57 +0100

    Refactoring

--- __tests__/FunctionalProgrammingExcercisesTests/FilterWithExercise.test.js
+++ __tests__/FunctionalProgrammingExcercisesTests/FilterWithExercise.test.js
@@ -1,11 +1,17 @@
 import { data } from '../../FunctionalProgrammingExcercises/FilterWithExerciseData';
 import * as filterWithExercise from '../../FunctionalProgrammingExcercises/FilterByStringExcercise.js';
+import {containsFilter} from "../../FunctionalProgrammingExcercises/FilterByStringExcercise.js";
 
 describe(`Tests for the function that checks if some particular element contains the filter string.`, () => {
     describe('Check if function returns correct results', () => {
+
+        const containsFilter = function(element) {
+            const defaultFilter = 'filter';
+            return filterWithExercise.containsFilter(element, defaultFilter);
+        }
+
         test(`Provided data is an object, array or array of objects, string and number. Should return true or false.`, () => {
-           const filter = 'filter';
-           const expectedTrue = [{ name: 'Susan', has: 'filter' },
+           const allExpectedToResultInTrueWithFilter = [{ name: 'Susan', has: 'filter' },
                ['sure', 'there', 'chocolate', 'hair', 'filter', 24],
                [{ someString: 'someString', someOtherString: 'fly' },
                    { someString: 'someString', someOtherString: 'another' },
@@ -13,8 +19,7 @@ describe(`Tests for the function that checks if some particular element contains
                    { someString: 'no', someOtherString: 'dance' },
                    { someString: 'like', someOtherString: 'forest' }],
                'filtered'];
-
-           const expectedFalse = [{ name: 'Susan', has: 'nothing' },
+           const allExpectedToResultInFalseWithFilter = [{ name: 'Susan', has: 'nothing' },
                ['sure', 'there', 'chocolate', 'hair', 'loose', 24],
                [{ someString: 'someString', someOtherString: 'fly' },
                    { someString: 'someString', someOtherString: 'another' },
@@ -23,28 +28,35 @@ describe(`Tests for the function that checks if some particular element contains
                    { someString: 'like', someOtherString: 'forest' }],
                'mighty'];
 
-            expectedTrue.forEach(element => expect(filterWithExercise.containsFilter(element, filter)).toBe(true));
-            expectedFalse.forEach(element => expect(filterWithExercise.containsFilter(element, filter)).toBe(false));
+           expect(allExpectedToResultInTrueWithFilter
+               .every(element => containsFilter(element))).toBe(true);
+
+           expect(allExpectedToResultInFalseWithFilter
+               .every(element => containsFilter(element))).toBe(true);
         });
 
         test(`Provided filter is a string that contains number. 
         In the provided data array there is the same number of type number, not a string.
         Should find it in the provided data and return true.`, () => {
-           const filter = '24';
-           const data = [['sure', 'there', 24, 'chocolate', 'hair', 'filter'],
+           const numberAsString = '24';
+           const nestedArraysOfDataWithNumbersInDifferentForms = [['sure', 'there', 24, 'chocolate', 'hair', 'filter'],
                ['sure', 'there', '24', 'chocolate', 'hair', 'filter'],
                ['sure', 'there', '24years old', 'chocolate', 'hair', 'filter']];
 
-            data.forEach(element =>  expect(filterWithExercise.containsFilter(element, filter)).toBe(true));
+           const containsNumber = function(element) {
+               return filterWithExercise.containsFilter(element, numberAsString);
+           }
+
+           expect(nestedArraysOfDataWithNumbersInDifferentForms
+               .every(element => containsNumber(element))).toBe(true);
         });
     });
 
     describe(`Check if function responds properly to encountered errors`, () => {
         test(`Provided data element is not an array, object, string or number. Should throw TypeError.`, () => {
-            const filter = 'filter';
-            const data = [true, NaN, null, undefined];
+            const typesArray = [true, NaN, null, undefined];
 
-            data.forEach(element => expect(() => filterWithExercise.containsFilter(element, filter)
+            typesArray.forEach(type => expect(() => containsFilter(type)
                 .toThrow(new TypeError(`Element is not a number, string, array, object or is null.`))));
         });
     });
@@ -52,31 +64,52 @@ describe(`Tests for the function that checks if some particular element contains
 
 
 describe('Tests for the function that should be used to get the final results.', () => {
+    const filterWithImportedData = function(filter) {
+        return filterWithExercise.filterWith(data, filter);
+    }
+
     describe('Check if function returns correct results', () => {
         test(`Filter can be found in the first (shallow) level of the data array.
         Should return array of only the objects that contain the filter.`, () => {
-            const filter1 = 'velos';
-            const filter2 = 'female';
+            const filterVelos = 'velos';
+            const filterFemale = 'female';
 
-            expect(filterWithExercise.filterWith(data, filter1)).toEqual([data[0]]);
-            expect(filterWithExercise.filterWith(data, filter2)).toEqual([data[1], data[3]]);
+            const resultFoundForVelosString = filterWithImportedData(filterVelos);
+            const resultFoundForFemaleString = filterWithImportedData(filterFemale);
+
+            const correctResultForVelosString = [data[0]];
+            const correctResultForFemaleString = [data[1], data[3]];
+
+            expect(resultFoundForVelosString).toEqual(correctResultForVelosString);
+            expect(resultFoundForFemaleString).toEqual(correctResultForFemaleString);
         });
 
         test(`Filter can be found in the deeper levels of the data array.
         Should return array of only the objects that contain the filter.`, () => {
-            const filter1 = 'sheppard';
-            const filter2 = 'culpa';
-            const filter3 = 'dolor';
+            const filterSheppard = 'sheppard';
+            const filterCulpa = 'culpa';
+            const filterDolor = 'dolor';
 
-            expect(filterWithExercise.filterWith(data, filter1)).toEqual([data[0]]);
-            expect(filterWithExercise.filterWith(data, filter2)).toEqual([data[4]]);
-            expect(filterWithExercise.filterWith(data, filter3)).toEqual([data[1], data[4]]);
+            const resultFoundForSheppardString = filterWithImportedData(filterSheppard);
+            const resultFoundForCulpaString = filterWithImportedData(filterCulpa);
+            const resultFoundForDolorString = filterWithImportedData(filterDolor);
+
+            const correctResultForSheppardString = [data[0]];
+            const correctResultFoundForCulpaString = [data[4]];
+            const correctResultFoundForDolorString = [data[1], data[4]];
+
+            expect(resultFoundForSheppardString).toEqual(correctResultForSheppardString);
+            expect(resultFoundForCulpaString).toEqual(correctResultFoundForCulpaString);
+            expect(resultFoundForDolorString).toEqual(correctResultFoundForDolorString);
         });
 
         test(`Filter length is less or equal to 2. Should return empty array.`, () => {
-            const filters = ['ex', '2', ''];
+            const filtersLengthLessEqual2 = ['ex', '2', ''];
 
-            filters.forEach(filter => expect(filterWithExercise.filterWith(data, filter)).toEqual([]));
+            const emptyArray = [];
+
+            filtersLengthLessEqual2.forEach(filter => expect(filterWithImportedData(filter))
+                .toEqual(emptyArray));
         });
     });
 
@@ -84,16 +117,16 @@ describe('Tests for the function that should be used to get the final results.',
     describe(`Check if function responds properly to encountered errors`, () => {
         test(`Provided argument is not of type array. Should throw TypeError.`, () => {
             const filter = 'filter';
-            const data = [true, NaN, null, undefined, 23, 'string'];
+            const dataTypes = [true, NaN, null, undefined, 23, 'string'];
 
-            data.forEach(element => expect(() => filterWithExercise.filterWith(element, filter)
+            dataTypes.forEach(type => expect(() => filterWithExercise.filterWith(type, filter)
                 .toThrow(new TypeError(`Provided array variable isn't of type array`))));
         });
 
         test(`Provided filter is not of type string or number. Should throw TypeError.`, () => {
-            const filters = [true, NaN, null, undefined, [], 2]
+            const filterTypes = [true, NaN, null, undefined, [], 2]
 
-            filters.forEach(filter => expect(() => filterWithExercise.filterWith(data, filter)
+            filterTypes.forEach(type => expect(() => filterWithImportedData(type)
                 .toThrow(new TypeError(`Provided filter is not a string neither a number.`))));
         });
     });
