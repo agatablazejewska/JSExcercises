c1f9d48c21f99f1144a49c2750e9dc1f6d322616
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-11-11 21:04:15 +0100

    Added error messages to make the class more user-friendly

--- OOPExcercises/Excercise2TS/DiscountCodes/DiscountCodes.ts
+++ OOPExcercises/Excercise2TS/DiscountCodes/DiscountCodes.ts
@@ -16,22 +16,34 @@ export class DiscountCodes implements IDiscountCodes {
     add(codeAndPercentageOff: codeAndPercentageOffType) : void {
         codeAndPercentageOff.percentOff = DiscountValidator.validateDiscountOrChangeToZero(codeAndPercentageOff.percentOff);
 
-        if(!this._isCodeAlreadyPresent(codeAndPercentageOff.code)) {
-            this._discountCodes.push(codeAndPercentageOff);
+        if(this._isCodeAlreadyPresent(codeAndPercentageOff.code)) {
+            console.error('The code already exists.');
+            return;
         }
+
+        this._discountCodes.push(codeAndPercentageOff);
     }
 
     remove(code: string): void {
         const index = this._discountCodes.findIndex(c => c.code === code);
 
-        if (index > -1) {
-            this._discountCodes.splice(index, 1);
+        if (index === -1) {
+            console.error('There is no such code in the list.');
+            return;
         }
+
+        this._discountCodes.splice(index, 1);
     }
 
     getPercentOff(code: string): number {
         const percentOff = this._discountCodes.find(c => c.code === code)?.percentOff;
-        return percentOff || 0;
+
+        if(!percentOff) {
+            console.error('There is no such discount code in the list.');
+            return 0;
+        }
+
+        return percentOff;
     }
 
     _isCodeAlreadyPresent(code: string) : boolean {
