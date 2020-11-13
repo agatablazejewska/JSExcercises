27758511c533530acd73f8278656ea9901a19ace
Agata Sokołowska <agata.blazejewska0@gmail.com>
2020-11-13 15:06:30 +0100

    All tests for the Cart class added

--- __tests__/OOPExercisesTests/OnlineShop/Cart.test.ts
+++ __tests__/OOPExercisesTests/OnlineShop/Cart.test.ts
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import { Cart } from '../../../OOPExcercises/Excercise2TS/Cart/Cart';
+import { DiscountCodesSingletone } from '../../../OOPExcercises/Excercise2TS/DiscountCodes/DiscountCodesSingletone';
 import { Item } from '../../../OOPExcercises/Excercise2TS/Item/Item';
 import { Categories } from '../../../OOPExcercises/Excercise2TS/Utilities/Enums/Categories';
 import { ItemAmountAndPrice } from '../../../OOPExcercises/Excercise2TS/Utilities/Interfaces/Cart/ICart';
+import { IItem } from '../../../OOPExcercises/Excercise2TS/Utilities/Interfaces/Item/IItem';
 
 const consoleErrorSpy = jest.spyOn(console, 'error');
 
@@ -54,7 +56,7 @@ describe(`Tests for the addItem method.`, () => {
             const correctFinalPrice = correctDataAssociatedWithItem50PercentDiscount.finalPrice + correctDataAssociatedWithItem0PercentDiscount.finalPrice;
 
             expect(cart.getAllItemsAmount()).toEqual(2);
-            expect(cart.getAllDiscountsSumPrice()).toEqual(correctDiscountsSummary);
+            expect(cart.getAllDiscountsSum()).toEqual(correctDiscountsSummary);
             expect(cart.getFinalPrice()).toEqual(correctFinalPrice);
         });
 
@@ -95,37 +97,33 @@ describe(`Tests for the removeOneItemOfThisType method.`, () => {
     describe('Check if method returns correct results', () => {
         test(`There is only one item of this type in the cart. 
         Should remove item from the cart and items list length should be back to 0 and cart summary should get updated.`, () => {
-            const correctDiscountAmount = item50PercentDiscount.price * (item50PercentDiscount.discount/100);
+            const correctDiscountAmount = item50PercentDiscount.price * (item50PercentDiscount.discount / 100);
             const correctCartsFinalPrice = item50PercentDiscount.getPriceAfterDiscount();
             cart.addItem(item50PercentDiscount);
 
             expect(cart.getAllItemsAmount()).toEqual(1);
             expect(cart.getFinalPrice()).toEqual(correctCartsFinalPrice);
-            expect(cart.getAllDiscountsSumPrice()).toEqual(correctDiscountAmount);
+            expect(cart.getAllDiscountsSum()).toEqual(correctDiscountAmount);
 
             cart.removeOneItemOfThisType(item50PercentDiscount.id);
             expect(cart.getAllItemsAmount()).toEqual(0);
             expect(cart.getFinalPrice()).toEqual(0);
-            expect(cart.getAllDiscountsSumPrice()).toEqual(0);
+            expect(cart.getAllDiscountsSum()).toEqual(0);
         });
 
-        test(`There are 3 items of this type in the cart. Should remove one of them and update cart's summary.`, () => {
+        test(`There are 3 items of this type in the cart. Should update cart's summary.`, () => {
             const otherItemToBeAdded = new Item('other', Categories.Alcohol, 23);
 
             const correctCartsFinalPriceAllItemsAdded = item50PercentDiscount.getPriceAfterDiscount() * 2
                 + otherItemToBeAdded.getPriceAfterDiscount();
-            const correctCartsDiscountAmountAllItemsAdded = 2 * (item50PercentDiscount.price * (item50PercentDiscount.discount/100))
-            + (otherItemToBeAdded.price * (otherItemToBeAdded.discount/100));
+            const correctCartsDiscountAmountAllItemsAdded = 2 * (item50PercentDiscount.price * (item50PercentDiscount.discount / 100))
+                + (otherItemToBeAdded.price * (otherItemToBeAdded.discount / 100));
 
             const correctItemListAllItemsAndAfterRemoval = [item50PercentDiscount, otherItemToBeAdded];
 
-            const correctCartsFinalPriceAfterRemoval = correctCartsFinalPriceAllItemsAdded -item50PercentDiscount.getPriceAfterDiscount();
-            const correctCartsDiscountAmountAfterRemoval = correctCartsDiscountAmountAllItemsAdded - (item50PercentDiscount.price * (item50PercentDiscount.discount/100));
-
-            function expectItemsToBeEqual(product: ItemAmountAndPrice, index: number) {
-                const areItemsEqual = _.isEqual(product.item, correctItemListAllItemsAndAfterRemoval[index])
-                expect(areItemsEqual).toBe(true);
-            }
+            const correctCartsFinalPriceAfterRemoval = correctCartsFinalPriceAllItemsAdded - item50PercentDiscount.getPriceAfterDiscount();
+            const correctCartsDiscountAmountAfterRemoval = correctCartsDiscountAmountAllItemsAdded -
+                (item50PercentDiscount.price * (item50PercentDiscount.discount / 100));
 
             cart.addItem(item50PercentDiscount);
             cart.addItem(item50PercentDiscount);
@@ -133,17 +131,19 @@ describe(`Tests for the removeOneItemOfThisType method.`, () => {
 
             expect(cart.getAllItemsAmount()).toEqual(3);
             expect(cart.getFinalPrice()).toEqual(correctCartsFinalPriceAllItemsAdded);
-            expect(cart.getAllDiscountsSumPrice()).toEqual(correctCartsDiscountAmountAllItemsAdded);
-            cart.items.forEach((product, index) =>  expectItemsToBeEqual(product, index));
+            expect(cart.getAllDiscountsSum()).toEqual(correctCartsDiscountAmountAllItemsAdded);
+            cart.items.forEach((product, index) =>
+                expectItemsToBeEqual(product, index, correctItemListAllItemsAndAfterRemoval));
 
             cart.removeOneItemOfThisType(item50PercentDiscount.id);
             expect(cart.getAllItemsAmount()).toEqual(2);
             expect(cart.getFinalPrice()).toEqual(correctCartsFinalPriceAfterRemoval);
-            expect(cart.getAllDiscountsSumPrice()).toEqual(correctCartsDiscountAmountAfterRemoval);
-            cart.items.forEach((product, index) =>  expectItemsToBeEqual(product, index));
+            expect(cart.getAllDiscountsSum()).toEqual(correctCartsDiscountAmountAfterRemoval);
+            cart.items.forEach((product, index) =>
+                expectItemsToBeEqual(product, index, correctItemListAllItemsAndAfterRemoval));
         });
     });
-    
+
     describe(`Check if method responds properly to encountered errors`, () => {
         test(`The item can't be located in the cart. 
         Should inform user through console.error.`, () => {
@@ -154,3 +154,123 @@ describe(`Tests for the removeOneItemOfThisType method.`, () => {
         });
     });
 });
+
+describe(`Tests for the removeAllItemsOfThisType method.`, () => {
+    describe('Check if method returns correct results', () => {
+        test(`There are two different items in the cart. 
+        Should remove one of them completely. Should update cart's summary.`, () => {
+            const otherItemToBeAdded = new Item('other', Categories.Alcohol, 23);
+
+            const correctCartsFinalPriceAllItemsAdded = item50PercentDiscount.getPriceAfterDiscount() * 2
+                + otherItemToBeAdded.getPriceAfterDiscount();
+            const correctCartsDiscountAmountAllItemsAdded = 2 * (item50PercentDiscount.price * (item50PercentDiscount.discount / 100))
+                + (otherItemToBeAdded.price * (otherItemToBeAdded.discount / 100));
+
+            const correctItemListAllItems = [item50PercentDiscount, otherItemToBeAdded];
+            const correctItemListAllItemsAfterRemoval = [otherItemToBeAdded];
+
+            const correctCartsFinalPriceAfterRemoval = correctCartsFinalPriceAllItemsAdded - (2 * item50PercentDiscount.getPriceAfterDiscount());
+            const correctCartsDiscountAmountAfterRemoval = correctCartsDiscountAmountAllItemsAdded -
+                2 * (item50PercentDiscount.price * (item50PercentDiscount.discount / 100));
+
+            cart.addItem(item50PercentDiscount);
+            cart.addItem(item50PercentDiscount);
+            cart.addItem(otherItemToBeAdded);
+
+            expect(cart.getAllItemsAmount()).toEqual(3);
+            expect(cart.getFinalPrice()).toEqual(correctCartsFinalPriceAllItemsAdded);
+            expect(cart.getAllDiscountsSum()).toEqual(correctCartsDiscountAmountAllItemsAdded);
+            expect(cart.items.length).toBe(2);
+            cart.items.forEach((product, index) =>
+                expectItemsToBeEqual(product, index, correctItemListAllItems));
+
+            cart.removeAllItemsOfThisType(item50PercentDiscount.id);
+
+            expect(cart.getAllItemsAmount()).toEqual(1);
+            expect(cart.getFinalPrice()).toEqual(correctCartsFinalPriceAfterRemoval);
+            expect(cart.getAllDiscountsSum()).toEqual(correctCartsDiscountAmountAfterRemoval);
+            expect(cart.items.length).toBe(1);
+            cart.items.forEach((product, index) =>
+                expectItemsToBeEqual(product, index, correctItemListAllItemsAfterRemoval));
+        });
+    });
+
+
+    describe(`Check if method responds properly to encountered errors`, () => {
+        test(`There are no items of this type in the list. 
+        Should inform user through console.error.`, () => {
+            cart.removeAllItemsOfThisType(item50PercentDiscount.id);
+
+            expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+            expect(consoleErrorSpy).toHaveBeenCalledWith(`There are no such items in this cart.`);
+        });
+    });
+});
+
+describe(`Tests for the applyDiscountCode method.`, () => {
+    test(`Should update final price after applying discount code.`, () => {
+        const discount50Percent = { code: '50Percent', percentOff: 50 };
+        const correctFinalPriceBeforeApplyingDiscountCode = 100;
+        const correctFinalPriceAfterApplyingDiscountCode = 50;
+        DiscountCodesSingletone.discountCodes.add(discount50Percent);
+
+        cart.addItem(item0PercentDiscount);
+        const finalPriceBeforeApplyingDiscountCode = cart.getFinalPrice();
+        expect(finalPriceBeforeApplyingDiscountCode).toEqual(correctFinalPriceBeforeApplyingDiscountCode);
+
+        cart.applyDiscountCode('50Percent');
+        expect(cart.getFinalPrice()).toEqual(correctFinalPriceAfterApplyingDiscountCode);
+    });
+});
+
+describe(`Tests for getters.`, () => {
+    test(`Test fot getFinalPrice. Should get correct value.`, () => {
+        const correctFinalPrice = 2 * item0PercentDiscount.getPriceAfterDiscount();
+
+        cart.addItem(item0PercentDiscount);
+        cart.addItem(item0PercentDiscount);
+
+        expect(cart.getFinalPrice()).toBe(correctFinalPrice);
+    });
+
+    test(`Test for getPriceNoDiscountCode. Should get price without the discount applied.`, () => {
+        const discount50Percent = { code: '50Percent', percentOff: 50 };
+        const correctPriceWithoutDiscountCodeApplied = 2 * item0PercentDiscount.getPriceAfterDiscount();
+
+        DiscountCodesSingletone.discountCodes.add(discount50Percent);
+
+        cart.addItem(item0PercentDiscount);
+        cart.addItem(item0PercentDiscount);
+        cart.applyDiscountCode('50Percent');
+
+        expect(cart.getPriceNoDiscountCode()).toBe(correctPriceWithoutDiscountCodeApplied);
+    });
+
+    test(`Test for getAllDiscountsSum. Should get all of the discounts summed up.`, () => {
+        cart.addItem(item50PercentDiscount);
+        cart.addItem(item50PercentDiscount);
+
+        const discount50Percent = { code: '50Percent', percentOff: 50 };
+        const discountAmountFromItems = 2 * (item50PercentDiscount.price - item50PercentDiscount.getPriceAfterDiscount());
+        const discountAmountFromItemsAndDiscountCode = discountAmountFromItems
+            + (cart.getFinalPrice() * discount50Percent.percentOff / 100);
+        DiscountCodesSingletone.discountCodes.add(discount50Percent);
+
+        cart.applyDiscountCode('50Percent');
+
+        expect(cart.getAllDiscountsSum()).toBe(discountAmountFromItemsAndDiscountCode);
+    });
+
+    test(`Test for the getAllItemsAmount method. Should return correct results.`, () => {
+        cart.addItem(item50PercentDiscount);
+        cart.addItem(item50PercentDiscount);
+        cart.addItem(item0PercentDiscount);
+
+        expect(cart.getAllItemsAmount()).toBe(3);
+    })
+});
+
+function expectItemsToBeEqual(product: ItemAmountAndPrice, index: number, arrayWithItemsToCompare: IItem[]) {
+    const areItemsEqual = _.isEqual(product.item, arrayWithItemsToCompare[index]);
+    expect(areItemsEqual).toBe(true);
+}
